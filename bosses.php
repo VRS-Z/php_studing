e0527a1a0d5739fadc6c79cf2ac8eb7a6981f24d
Vrs <137067506+VRS-Z@users.noreply.github.com>
2025-12-19 18:27:48 -0300

    Fix logout link path in bosses.php

--- bosses.php
+++ bosses.php
@@ -23,7 +23,7 @@ require_once("auth.php");
                 <li><a href="home.php">Home</a></li>
                 <li><a href="sobre.php">Sobre</a></li>
                 <li><a href="contato.php">Contato</a></li>
-                <li><a href="/logout.php">Encerrar Sessão</a></li>
+                <li><a href="logout.php">Encerrar Sessão</a></li>
             </ul>
         </nav>
     </header>
